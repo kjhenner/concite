14b359e300b1a954b6107aefb7715c2da6e5c2d5
kjhenner <kjhenner@gmail.com>
2019-07-25 23:51:01 -0400

    add venue_classifier model test and config

---
+++ tests/models/test_venue_classifier.py
@@ -0,0 +1,23 @@
+from allennlp.common.testing import ModelTestCase
+from allennlp.common.util import ensure_list
+
+import sys
+
+from pathlib import Path
+
+sys.path.append(str(Path('.').absolute()))
+
+from concite.models.venue_classifier import VenueClassifier
+from concite.dataset_readers.pubmed_document_dataset_reader import PubmedDocumentDatasetReader
+
+class VenueClassifierTest(ModelTestCase):
+    
+    def setUp(self):
+        super(VenueClassifierTest, self).setUp()
+        self.set_up_model('tests/fixtures/test_config.json',
+                          'tests/fixtures/n2v_pubmed_articles.jsonl')
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+
+
