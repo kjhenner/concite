444e87f83a1a8a60d37632677ac5dfe3405e179d
kjhenner <kjhenner@gmail.com>
2019-07-25 23:50:15 -0400

    add venue_classifier model

---
+++ concite/models/venue_classifier.py
@@ -0,0 +1,75 @@
+
+from typing import Dict, Optional
+
+import allennlp
+import numpy as np
+import torch
+import torch.nn.functional as F
+from allennlp.common import Params
+from allennlp.data import Instance
+from allennlp.data import Vocabulary
+from allennlp.data.dataset import Batch
+from allennlp.data.fields import TextField, LabelField
+from allennlp.data.token_indexers import SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+from allennlp.models.model import Model
+from allennlp.modules import FeedForward, TextFieldEmbedder, Seq2VecEncoder
+from allennlp.nn import InitializerApplicator, RegularizerApplicator
+from allennlp.nn import util
+from allennlp.training.metrics import CategoricalAccuracy, F1Measure
+from overrides import overrides
+
+@Model.register("venue_classifier")
+class VenueClassifier(Model):
+    def __init__(self,
+                 vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 abstract_encoder: Seq2VecEncoder,
+                 classifier_feedforward: FeedForward,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super(VenueClassifier, self).__init__(vocab, regularizer)
+
+        self.text_field_embedder = text_field_embedder
+        self.num_classes = self.vocab.get_vocab_size("labels")
+        self.abstract_encoder = abstract_encoder
+        self.classifier_feedforward = classifier_feedforward
+        self.metrics = {
+                "accuracy": CategoricalAccuracy(),
+                "accuracy3": CategoricalAccuracy(top_k=3)
+        }
+        self.loss = torch.nn.CrossEntropyLoss()
+        initializer(self)
+
+    def forward(self,
+                abstract: Dict[str, torch.LongTensor],
+                graph_vector: torch.LongTensor,
+                venue: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        embedded_abstract = self.text_field_embedder(abstract)
+        abstract_mask = util.get_text_field_mask(abstract)
+        encoded_abstract = self.abstract_encoder(embedded_abstract, abstract_mask)
+
+        logits = self.classifier_feedforward(torch.cat([encoded_abstract, graph_vector], dim=-1))
+        class_probabilities = F.softmax(logits)
+
+        output_dict = {"class_probabilities": class_probabilities}
+
+        if label is not None:
+            loss = self.loss(logits, label.squeeze(-1))
+            for metric in self.metrics.value():
+                metric(logits, label.squeeze(-1))
+            output_dict["loss"] = loss
+
+        return output_dict
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
+
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        predictions = output_dict['class_probabilities'].cpu().data.numpy()
+        argmax_indices = numpy.argmax(predictions, axis=-1)
+        labels = [self.vocab.get_token_from_index(x, namespace="labels")
+                for x in argmax_indices]
+        output_dict['label'] = labels
+        return output_dict
+
