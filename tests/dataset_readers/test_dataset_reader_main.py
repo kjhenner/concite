447baf09e97fd647f857a42f92f86c6f7be7550f
kjhenner <kjhenner@gmail.com>
2019-07-25 23:19:19 -0400

    add PubmedDocumentDatasetReader() test

--- tests/dataset_readers/test_dataset_reader_main.py
+++ tests/dataset_readers/test_dataset_reader_main.py
@@ -8,9 +8,16 @@ from pathlib import Path
 sys.path.append(str(Path('.').absolute()))
 
 from concite.dataset_readers.aclarc_data_reader import AclarcDocDatasetReader
+from concite.dataset_readers.pubmed_document_dataset_reader import PubmedDocumentDatasetReader
 
 class TestDatasetReader(AllenNlpTestCase):
-    def test_read_from_file(self):
+
+    def test_read_from_pubmed_file(self):
+        reader = PubmedDocumentDatasetReader()
+        instances = ensure_list(reader.read('tests/fixtures/n2v_pubmed_articles.jsonl'))
+        assert isinstance(instances, list)
+
+    def test_read_from_aclarc_file(self):
         reader = AclarcDocDatasetReader()
         instances = ensure_list(reader.read('tests/fixtures/aclarc-train.jsonl'))
         assert isinstance(instances, list)
