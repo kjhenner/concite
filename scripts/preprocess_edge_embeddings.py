e8c4396ff6289aec7861647631f556b270ac11a9
kjhenner <kjhenner@gmail.com>
2019-07-28 17:57:00 -0400

    update preprocess script to use graph_tool CitGraph

--- scripts/preprocess_edge_embeddings.py
+++ scripts/preprocess_edge_embeddings.py
@@ -1,21 +1,13 @@
 import sys
-import networkx as nx
-import jsonlines
 from pathlib import Path
 sys.path.append(str(Path('.').absolute()))
 
 from concite.dataset_preprocessors import citation_graph
 
-edge_path = sys.argv[1]
-node_path = sys.argv[2]
-output_path = sys.argv[3]
+data_path = sys.argv[1]
+graph_out_path = sys.argv[2]
 
-G = citation_graph.CitGraph()
-G.load_edges(edge_path)
-G.filter_by_degree()
-
-emb = G.embed_edges(G.edges(), use_cache=True)
-
-with jsonlines.open(node_path) as reader:
-    with jsonlines.open(output_path, 'w') as writer:
-        writer.write_all(map(lambda x: dict({'n2v_vector':list(emb.to_vec(x['pmid']))}, **x), [ex for ex in reader if ex['pmid'] in G]))
+g = citation_graph.CitGraph()
+g.load_data_from_dir(data_path)
+g.embed_edges(list(g.edges()), use_cache=False)
+g.save(graph_out_path)
