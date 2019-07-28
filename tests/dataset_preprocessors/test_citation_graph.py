ba3f9438627d4f1edd614704f0bec01122ffb267
kjhenner <kjhenner@gmail.com>
2019-07-28 17:57:43 -0400

    add tests and fixtures for citation graph

---
+++ tests/dataset_preprocessors/test_citation_graph.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+from pathlib import Path
+
+sys.path.append(str(Path('.').absolute()))
+
+from concite.dataset_preprocessors.citation_graph import CitGraph
+
+class TestCitGraph(unittest.TestCase):
+
+    def setUp(self):
+        self.g = CitGraph()
+        self.g.load_data_from_dir('tests/fixtures/cit_graph_test_data/')
+
+    def test_vertex_and_edge_counts(self):
+        self.assertEqual(len([v for v in self.g.vertices() if self.g.vp.abstract[v]]), 10)
+        self.assertEqual(len([v for v in self.g.vertices() if not self.g.vp.abstract[v]]), 3)
+        self.assertEqual(len(list(self.g.edges())), 23)
+
+    def test_embedding(self):
+        e_pairs = [tuple(e) for e in self.g.get_edges()[:,:2]]
+        self.g.embed_edges(e_pairs, use_cache=False)
+        self.assertEqual(
+                len([v for v in self.g.vertices() if self.g.vp.graph_vector[v]]),
+                self.g.num_vertices())
