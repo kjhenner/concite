7eceb4a03f0752e33a7bc46a3a237ae39b9ed08f
kjhenner <kjhenner@gmail.com>
2019-07-30 17:17:52 -0400

    Use SciBERT embeddings

--- concite/models/venue_classifier.py
+++ concite/models/venue_classifier.py
@@ -24,52 +24,56 @@ class VenueClassifier(Model):
     def __init__(self,
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
-                 abstract_encoder: Seq2VecEncoder,
                  classifier_feedforward: FeedForward,
+                 dropout: float = 0.2,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super(VenueClassifier, self).__init__(vocab, regularizer)
 
         self.text_field_embedder = text_field_embedder
-        self.num_classes = self.vocab.get_vocab_size("venues")
-        self.abstract_encoder = abstract_encoder
+        self.dropout = torch.nn.Dropout(dropout)
+        self.num_classes = self.vocab.get_vocab_size("labels")
         self.classifier_feedforward = classifier_feedforward
-        self.metrics = {
-                "accuracy": CategoricalAccuracy(),
-                "accuracy3": CategoricalAccuracy(top_k=3)
-        }
+
+        self.label_accuracy = CategoricalAccuracy()
+        self.label_f1_metrics = {}
+
+        for i in range(self.num_classes):
+            self.label_f1_metrics[vocab.get_token_from_index(index=1, namespace="labels")] = F1Measure(positive_label=1)
+
         self.loss = torch.nn.CrossEntropyLoss()
+
         initializer(self)
 
+    @overrides
     def forward(self,
                 abstract: Dict[str, torch.LongTensor],
                 graph_vector: torch.LongTensor,
-                venue: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+                label: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+
         embedded_abstract = self.text_field_embedder(abstract)
-        abstract_mask = util.get_text_field_mask(abstract)
-        encoded_abstract = self.abstract_encoder(embedded_abstract, abstract_mask)
+        pooled = self.dropout(embedded_abstract[:, 0, :])
+        logits = self.classifier_feedforward(torch.cat([pooled, graph_vector], dim=-1))
 
-        logits = self.classifier_feedforward(torch.cat([encoded_abstract, graph_vector], dim=-1))
-        class_probabilities = F.softmax(logits, dim=1)
+        class_probs = F.softmax(logits, dim=1)
 
-        output_dict = {"class_probabilities": class_probabilities}
+        output_dict = {"logits": logits}
 
-        if venue is not None:
-            loss = self.loss(logits, venue)
-            for metric in self.metrics.values():
-                metric(logits, venue)
+        if label is not None:
+            loss = self.loss(logits, label)
             output_dict["loss"] = loss
 
+            for i in range(self.num_classes):
+                metric = self.label_f1_metrics[self.vocab.get_token_from_index(index=1, namespace="labels")]
+                metric(class_probs, label)
+            self.label_accuracy(logits, label)
+
         return output_dict
 
-    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
-        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
-
-    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        predictions = output_dict['class_probabilities'].cpu().data.numpy()
-        argmax_indices = numpy.argmax(predictions, axis=-1)
-        venues = [self.vocab.get_token_from_index(x, namespace="venues")
-                for x in argmax_indices]
-        output_dict['venue'] = venues
-        return output_dict
-
+#    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+#        predictions = output_dict['class_probabilities'].cpu().data.numpy()
+#        argmax_indices = numpy.argmax(predictions, axis=-1)
+#        labels = [self.vocab.get_token_from_index(x, namespace="labels")
+#                for x in argmax_indices]
+#        output_dict['label'] = labels
+#        return output_dict
