a3fda0201e9511c06adec758d0543b22b48914d5
kjhenner <kjhenner@gmail.com>
2019-10-10 15:54:11 -0400

    finish and configure skip-gram model

--- concite/models/negative_sampling_model.py
+++ concite/models/negative_sampling_model.py
@@ -5,6 +5,7 @@ import allennlp
 import numpy as np
 import torch
 import torch.nn.functional as F
+import math
 from allennlp.common import Params
 from allennlp.data import Instance
 from allennlp.data import Vocabulary
@@ -13,7 +14,7 @@ from allennlp.data.fields import TextField, LabelField
 from allennlp.data.token_indexers import SingleIdTokenIndexer
 from allennlp.data.tokenizers import Token
 from allennlp.models.model import Model
-from allennlp.modules import FeedForward, TextFieldEmbedder, Seq2VecEncoder
+from allennlp.modules import FeedForward, TextFieldEmbedder, TokenEmbedder, Embedding
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
 from allennlp.training.metrics import CategoricalAccuracy, F1Measure
@@ -22,21 +23,38 @@ from overrides import overrides
 @Model.register("negative_sampling_model")
 class NegativeSamplingModel(Model):
     def __init__(self, vocab: Vocabulary,
-                 text_field_embedder: TextFieldEmbedder,
                  verbose_metrics: False,
                  embedding_dim: int = 128,
                  dropout: float = 0.2,
-                 window: int = 10,
+                 neg_samples: int = 10,
+                 cuda_device: int = 7,
+                 pretrained_file: str = None,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super(NegativeSamplingModel, self).__init__(vocab, regularizer)
 
-        self._window = window
-
-        self.text_field_embedder = text_field_embedder
+        self.embedder = Embedding(num_embeddings=vocab.get_vocab_size('source_token'),
+                                   embedding_dim=embedding_dim,
+                                   pretrained_file=pretrained_file)
+        self.neg_samples = neg_samples
+        self.cuda_device = cuda_device
         self.dropout = torch.nn.Dropout(dropout)
-
         self.verbose_metrics = verbose_metrics
+
+        # Compute negative sampling probabilities
+        # Based on https://github.com/mhagiwara/realworldnlp
+        token_probs = {}
+        token_counts = vocab._retained_counter['source_token']
+        total_counts = float(sum(token_counts.values()))
+        total_probs = 0.
+        for token, counts in token_counts.items():
+            adjusted_freq = math.pow(counts / total_counts, 0.75)
+            token_probs[token] = adjusted_freq
+            total_probs += adjusted_freq
+
+        self.neg_sample_probs = np.ndarray((vocab.get_vocab_size('source_token'),))
+        for idx, token in vocab.get_index_to_token_vocabulary('source_token').items():
+            self.neg_sample_probs[idx] = token_probs.get(token, 0) / total_probs
 
         initializer(self)
 
@@ -52,18 +70,26 @@ class NegativeSamplingModel(Model):
 
     @overrides
     def forward(self,
-                walk: List[Dict[str, torch.LongTensor]]) -> Dict[str, torch.Tensor]:
+                source_token: Dict[str, torch.LongTensor],
+                nbr_token: Dict[str, torch.LongTensor]) -> Dict[str, torch.Tensor]:
+        batch_size = source_token.shape[0]
 
-        output_dict = {}
-        embedded = self.text_field_embedder(sequence)
-        for sentence in embedded:
-            for pos, word in sentence:
-                start = max(0, pos - self._window)
-                for pos2, word2 in enumerate(sentence[start:(pos + model.window + 1)], start):
-                    if pos2 != pos:
-                        pass
-        return output_dict
+        
+        embedded_source = self.embedder(source_token)
+        embedded_nbr = self.embedder(nbr_token)
 
-        #loss = self._compute_loss(embedded)
-        #output_dict["loss"] = loss
+        log_prob = F.logsigmoid(torch.mul(embedded_source, embedded_nbr).sum(dim=1))
 
+        negative_nbr = np.random.choice(a=self.vocab.get_vocab_size('source_token'),
+                                        size=batch_size * self.neg_samples,
+                                        p=self.neg_sample_probs)
+        negative_nbr = torch.LongTensor(negative_nbr).view(batch_size, self.neg_samples)
+        if self.cuda_device > -1:
+            negative_nbr = negative_nbr.to(self.cuda_device)
+
+        embedded_neg_nbr = self.embedder(negative_nbr)
+        negative = torch.bmm(embedded_neg_nbr, embedded_source.unsqueeze(2)).squeeze()
+
+        log_prob += F.logsigmoid(-1. * negative).sum(dim=1)
+
+        return {'loss': -log_prob.sum() / batch_size}
