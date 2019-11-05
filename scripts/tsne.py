4335a4ab1f255dd5675b4bd7a581c4d5281c3a1e
kjhenner <kjhenner@gmail.com>
2019-11-05 02:30:32 -0500

    adding tsne visualization script

--- scripts/tsne.py
+++ scripts/tsne.py
@@ -2,20 +2,45 @@ import sys
 import os
 import numpy as np
 import matplotlib
+from mpl_toolkits import mplot3d
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
+from collections import defaultdict
+import jsonlines
+
+def get_workshop_counts(file_path, workshop_lookup):
+    workshop_counts = defaultdict(int)
+    for ex in jsonlines.open(file_path):
+        workshop = workshop_lookup.get(ex['paper_id'][:6])
+        if workshop:
+            workshop_counts[workshop] += 1
+    workshop_counts = list(workshop_counts.items())
+    workshop_counts.sort(key=lambda x: -x[1])
+    return workshop_counts
 
 def tsne_plot(labels, vectors, out_path):
     tsne = TSNE(n_components=2, random_state=0).fit_transform(vectors)
     matplotlib.rc('font', size=3)
-    fig, axes = plt.subplots(nrows=2, ncols=5, dpi=300, figsize=(7.5,3))
+    fig, axes = plt.subplots(nrows=4, ncols=6, dpi=400, figsize=(12,8))
     for i, label in enumerate(set(labels)):
-        c = [7-float(l == label)*3 for l in labels]
-        plt.subplot(2, 5, i+1)
-        plt.scatter(tsne[:, 0], tsne[:, 1], s=1, c=c, linewidths=0, cmap='tab20c')
+        c = np.asarray([(0.8, 0.1, 0.2, 0.8) if l==label else (0.6,0.6,0.6,0.2) for l in labels])
+        plt.subplot(4, 6, i+1)
+        plt.scatter(tsne[:, 0], tsne[:, 1], s=1, c=c, linewidths=0)
         plt.title(label, pad=-4)
         plt.axis('off')
+    fig.tight_layout()
+    plt.savefig(out_path)
+
+def tsne_plot_3d(labels, vectors, dates, out_path):
+    tsne = TSNE(n_components=2, random_state=0).fit_transform(vectors)
+    matplotlib.rc('font', size=3)
+    fig, axes = plt.subplots(nrows=2, ncols=5, dpi=400, figsize=(10,4))
+    for i, label in enumerate(set(labels)):
+        c = np.asarray([(0.8, 0.1, 0.2, 0.8) if l==label else (0.6,0.6,0.6,0.2) for l in labels])
+        plt.subplot(2, 5, i+1, projection='3d')
+        plt.scatter(tsne[:, 0], tsne[:, 1], s=1, c=c, linewidths=0)
+        plt.title(label, pad=-4)
     fig.tight_layout()
     plt.savefig(out_path)
 
@@ -28,6 +53,14 @@ def get_workshop_lookup(workshop_path):
                 workshop_lookup[paper_id] = items[1]
     return workshop_lookup
 
+def get_date_lookup(date_path):
+    date_lookup = {}
+    with open(date_path) as f:
+        for line in f.readlines():
+            items = line.split('\t')
+            date_lookup[items[0]] = items[1]
+    return date_lookup
+
 def get_emb_lookup(emb_path):
     emb_lookup = {}
     with open(emb_path) as f:
@@ -39,24 +72,17 @@ def get_emb_lookup(emb_path):
 if __name__ == "__main__":
     emb_path = sys.argv[1]
     workshop_path = sys.argv[2]
-    out_path = sys.argv[3]
+    date_path = sys.argv[3]
+    out_path = sys.argv[4]
     emb_lookup = get_emb_lookup(emb_path)
     workshop_lookup = get_workshop_lookup(workshop_path)
+    date_lookup = get_date_lookup(date_path)
 
-    top_workshops = [
-        "Machine Translation",
-        "Discourse and Dialogue",
-        "Chinese Language Processing",
-        "CoNLL",
-        "Proceedings of the SIGDIAL Conference",
-        "Linguistic Annotation Workshop",
-        "Innovative Use of NLP for Building Educational Applications",
-        "NLG",
-        "Proceedings of BioNLP Workshop",
-        "International Conference on Computational Semantics (IWCS)"
-    ]
+    workshop_counts = get_workshop_counts('/shared-1/projects/concite/data/acl_n2v.jsonl', workshop_lookup)
 
-    emb_lookup = {k:v for k,v in emb_lookup.items() if workshop_lookup.get(k[:6]) and workshop_lookup[k[:6]] in top_workshops}
+    top_workshops = [w[0] for w in workshop_counts[:10]]
+
+    emb_lookup = {k:v for k,v in emb_lookup.items() if workshop_lookup.get(k[:6]) and workshop_lookup[k[:6]] in top_workshops and k in date_lookup.keys()}
 
     vocabulary = {
         "workshop_labels": {label: idx for idx, label in enumerate(set(workshop_lookup.values()))},
@@ -68,4 +94,7 @@ if __name__ == "__main__":
     emb_matrix = np.array(list(emb_lookup.values()))
     labels = [workshop_lookup[paper_id[:6]]
         for paper_id in emb_lookup.keys()]
-    tsne_plot(labels, emb_matrix, out_path)
+    dates = [date_lookup.get(paper_id, -1)
+        for paper_id in emb_lookup.keys()]
+    #tsne_plot(labels, emb_matrix, out_path)
+    tsne_plot_3d(labels, emb_matrix, dates, out_path)
