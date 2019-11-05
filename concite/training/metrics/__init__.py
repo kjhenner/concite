9ab23f1f2a076593b263732781e18e7616a3e288
kjhenner <kjhenner@gmail.com>
2019-11-05 12:09:14 -0500

    initial work on predictors/confusion matrix

--- concite/training/metrics/__init__.py
+++ concite/training/metrics/__init__.py
@@ -1,2 +1,3 @@
 from concite.training.metrics.perplexity import Perplexity
 from concite.training.metrics.recall_at_n import RecallAtN
+from concite.training.metrics.confusion_matrix import ConfusionMatrix
